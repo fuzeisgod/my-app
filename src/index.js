45d68db81130a305537e7727d6041b2d4e18ffb2
chenzixin <17826806525@163.com>
2021-02-20 17:20:07 +0800

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,96 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// ...children: ...rest参数语法，保证 children 始终是一个数组
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      // 对象扩展
+      ...props,
+      // 将 children 中的字符等基本值类型的成员 转化为对象形式
+      children: children.map(child => {
+        return typeof child === "object"
+          ?
+          child
+          :
+          createTextElement(child)
+      })
+    }
+  }
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// 将不是对象的内容封装到它自己的元素，创建一个特殊类型 TEXT_element
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function createDom(fiber) {
+  // creating the dom node using the `element.type`
+  // if `element.type` is `TEXT_ELEMENT`, create a text node instead of a regular node
+  const dom =
+    fiber.type === "TEXT_ELEMENT"
+      ?
+      document.createTextNode("")
+      :
+      document.createElement(fiber.type)
+
+  // 为节点分配除了 children 的 props 属性
+  const isProperty = key => key !== "children"
+  Object.keys(fiber.props).filter(isProperty).forEach(name => {
+    dom[name] = fiber.props[name]
+  })
+
+  return dom
+}
+
+function render(element, container) {
+  // TODO set next unit of work
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element]
+    }
+  }
+}
+
+let nextUnitOfWork = null;
+
+function workLoop(deadLine) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadLine.timeRemaining() < 1
+  }
+  // make a loop, 主线程空闲时 run the callback
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+// 执行工作，返回下一个工作单元
+function performUnitOfWork(nextUnitOfWork) {
+  // TODO add dom node
+  // TODO create new fibers
+  // TODO return next unit
+}
+
+const Chen = {
+  createElement,
+  render
+}
+
+// 如果我们有一个类似这样的注释，当 babel 发送 JSX 时，它将使用我们定义的函数。
+/** @jsx Chen.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+)
+
+const container = document.getElementById("root")
+Chen.render(element, container)
